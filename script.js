b1afb3cd728508123c2e19ff5b2e37ba6276e7ea
johnken915 <63950642+johnken915@users.noreply.github.com>
2024-03-26 19:23:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
+yesBtn.addEventListener('click', () => {
+   // Change the content of the question
+   // Simulate a delay before redirecting (you can adjust the delay time as needed)
+   setTimeout(() => {
+       // Redirect to the next page
+       window.location.href = 'page14.html';
+   }, 0); // 2000 milliseconds delay
+});
+noBtn.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+    const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
+    noBtn.style.left = i + 'px';
+    noBtn.style.top = j + 'px';
+});
